084ead8507a21ee50ba130370351f85b7f82eccd
enliple <skkim@enliple.com>
2019-10-17 16:11:29 +0900

    chat activity theme 변경

--- ibotsdk/src/main/java/com/enliple/ibotsdk/activity/IBotSDKChatActivity.java
+++ ibotsdk/src/main/java/com/enliple/ibotsdk/activity/IBotSDKChatActivity.java
@@ -3,6 +3,8 @@ package com.enliple.ibotsdk.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -14,12 +16,14 @@ import com.enliple.ibotsdk.R;
 import com.enliple.ibotsdk.network.IBotURL;
 
 public class IBotSDKChatActivity extends AppCompatActivity {
-
+    public static final String IBOT_JAVASCRIPT_NAME = "iBotAppHandler";
     public static final String INTENT_API_KEY = "INTENT_API_KEY";
 
     private WebView webView = null;
 
     private String mallId = null;
+    private String loadUrl = "";
+    private String finishedUrl = "";
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
@@ -31,17 +35,41 @@ public class IBotSDKChatActivity extends AppCompatActivity {
         webView = findViewById(R.id.webView);
 
         WebChromeClient webChromeClient = new WebChromeClient();
-        WebViewClient webViewClient = new WebViewClient();
         WebSettings settings = webView.getSettings();
         webView.setWebChromeClient(webChromeClient);
-        webView.setWebViewClient(webViewClient);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if ( !TextUtils.isEmpty(loadUrl) ) {
+                    if ( url.equals(loadUrl) ) {
+                        if ( !url.equals(finishedUrl) ) {
+                            finishedUrl = url;
+                            webView.loadUrl("javascript:window.innerHeight");
+                        }
+                    }
+                }
+            }
+        });
         settings.setJavaScriptEnabled(true);
+
+        webView.addJavascriptInterface(new IBotJavascriptInterface(), IBOT_JAVASCRIPT_NAME);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        if ( mallId != null && !TextUtils.isEmpty(mallId) )
-            webView.loadUrl(IBotURL.CHAT_URL + mallId);
+        if ( mallId != null && !TextUtils.isEmpty(mallId) ) {
+            loadUrl = IBotURL.CHAT_URL + mallId;
+            webView.loadUrl(loadUrl);
+        }
+
+    }
+
+    public class IBotJavascriptInterface {
+        @JavascriptInterface
+        public void onAppViewClose() {
+            System.out.println("onAppViewClose called");
+            finish();
+        }
     }
 }
